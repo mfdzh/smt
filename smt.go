dc1a246aebff1afad80ab65b813ad636bafdcbda
dzh <1051072440@qq.com>
2022-01-17 10:49:16 +0800

    modify get operate

--- smt.go
+++ smt.go
@@ -91,7 +91,10 @@ func (smt *SparseMerkleTree) Get(key []byte) ([]byte, error) {
 		return defaultValue, nil
 	}
 
-	_, valueHash := smt.th.parseLeaf(leafData)
+	keyHash, valueHash := smt.th.parseLeaf(leafData)
+	if !bytes.Equal(keyHash, path) {
+		return defaultValue, nil
+	}
 
 	value, err := smt.values.Get(valueHash)
 	if err != nil {
@@ -124,7 +127,10 @@ func (smt *SparseMerkleTree) GetFromRoot(key, root []byte) ([]byte, error) {
 		return defaultValue, nil
 	}
 
-	_, valueHash := smt.th.parseLeaf(leafData)
+	keyHash, valueHash := smt.th.parseLeaf(leafData)
+	if !bytes.Equal(keyHash, path) {
+		return defaultValue, nil
+	}
 
 	value, err := smt.values.Get(valueHash)
 	if err != nil {
@@ -453,7 +459,10 @@ func (smt *SparseMerkleTree) GetFromPath(root []byte, path []byte) ([]byte, erro
 		return defaultValue, nil
 	}
 
-	_, valueHash := smt.th.parseLeaf(leafData)
+	keyHash, valueHash := smt.th.parseLeaf(leafData)
+	if !bytes.Equal(keyHash, path) {
+		return defaultValue, nil
+	}
 
 	value, err := smt.values.Get(valueHash)
 	if err != nil {
